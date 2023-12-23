6d33d77d27a9ca18ecb47d9b0861854adf10e933
Carlos Quinones <cquinones100@gmail.com>
2023-12-23 04:16:57 -0500

    Add identified_by to connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,5 +2,14 @@
 
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :robot_id
+
+    def connect
+      url = env['REQUEST_URI']
+
+      robot_id = url.split('/').last
+
+      self.robot_id = (robot_id.presence || 'dashboard')
+    end
   end
 end
